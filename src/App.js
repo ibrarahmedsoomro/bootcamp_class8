90ec41a63cb4945bf976be35026498052fbc19ec
ibrarsoomro <50750853+ibrarsoomro@users.noreply.github.com>
2020-09-07 10:05:17 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [repos, setRepos] = useState([{}]);
+
+  useEffect(() => {
+
+    async function getRepos() {
+      const response = await fetch("https://api.github.com/users/ibrarsoomro/repos")
+      const data = await response.json();
+      console.log(data);
+      setRepos(data)
+    }
+    getRepos();
+
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>My Repositaries</h1>
+      <ul>
+        {repos.map((repoObj, ind) => {
+          return (<li key={ind}>{repoObj.name}</li>)
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
